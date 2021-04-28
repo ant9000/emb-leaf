2e00b8acd5e2e0d207c1590d64e8aeb819820fab
Antonio Galea <antonio.galea@gmail.com>
2021-04-28 19:32:52 +0200

    better debug for SAML21 registers

---
+++ debug_saml21.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+#include "periph/rtt.h"
+#include "periph/pm.h"
+
+#include "board.h"
+
+void debug_saml21(void)
+{
+    puts("Oscillators:");
+    if (OSCCTRL->XOSCCTRL.bit.ENABLE) {
+        printf(" OSCCTRL->XOSCMCTRL");
+        if(OSCCTRL->XOSCCTRL.bit.ONDEMAND) { printf(" ONDEMAND"); }
+        if(OSCCTRL->XOSCCTRL.bit.RUNSTDBY) { printf(" RUNSTDBY"); }
+        if(OSCCTRL->XOSCCTRL.bit.XTALEN) { printf(" XTALEN"); }
+        puts("");
+    }
+    if (OSCCTRL->OSC16MCTRL.bit.ENABLE) {
+        char *osc16m_freqs[] = { "4MHz", "8MHz", "12MHz", "16MHz" };
+        printf(" OSCCTRL->OSC16MCTRL %s", osc16m_freqs[OSCCTRL->OSC16MCTRL.bit.FSEL]);
+        if(OSCCTRL->OSC16MCTRL.bit.ONDEMAND) { printf(" ONDEMAND"); }
+        if(OSCCTRL->OSC16MCTRL.bit.RUNSTDBY) { printf(" RUNSTDBY"); }
+        puts("");
+    }
+    if (OSCCTRL->DFLLCTRL.bit.ENABLE) {
+        printf(" OSCCTRL->DFLLCTRL 48MHz");
+        if(OSCCTRL->DFLLCTRL.bit.WAITLOCK) { printf(" WAITLOCK"); }
+        if(OSCCTRL->DFLLCTRL.bit.ONDEMAND) { printf(" ONDEMAND"); }
+        if(OSCCTRL->DFLLCTRL.bit.RUNSTDBY) { printf(" RUNSTDBY"); }
+        if(OSCCTRL->DFLLCTRL.bit.USBCRM) { printf(" USBCRM"); }
+        if(OSCCTRL->DFLLCTRL.bit.LLAW) { printf(" LLAW"); }
+        if (OSCCTRL->DFLLCTRL.bit.MODE) { printf(" CLOSED-LOOP"); }
+        puts("");
+    }
+    if (OSCCTRL->DPLLCTRLA.bit.ENABLE ) {
+        printf(" OSCCTRL->DPLLCTRLA");
+        if(OSCCTRL->DPLLCTRLA.bit.ONDEMAND) { printf(" ONDEMAND"); }
+        if(OSCCTRL->DPLLCTRLA.bit.RUNSTDBY) { printf(" RUNSTDBY"); }
+        puts("");
+    }
+    if (OSC32KCTRL->XOSC32K.bit.ENABLE) {
+        printf(" OSC32KCTRL->XOSC32K");
+        if(OSC32KCTRL->XOSC32K.bit.ONDEMAND) { printf(" ONDEMAND"); }
+        if(OSC32KCTRL->XOSC32K.bit.RUNSTDBY) { printf(" RUNSTDBY"); }
+        if(OSC32KCTRL->XOSC32K.bit.EN1K) { printf(" EN1K"); }
+        if(OSC32KCTRL->XOSC32K.bit.EN32K) { printf(" EN32K"); }
+        if(OSC32KCTRL->XOSC32K.bit.XTALEN) { printf(" XTALEN"); }
+        if(OSC32KCTRL->XOSC32K.bit.WRTLOCK) { printf(" WRTLOCK"); }
+        puts("");
+    }
+    if (OSC32KCTRL->OSC32K.bit.ENABLE) {
+        printf(" OSC32KCTRL->OSC32K");
+        if(OSC32KCTRL->OSC32K.bit.ONDEMAND) { printf(" ONDEMAND"); }
+        if(OSC32KCTRL->OSC32K.bit.RUNSTDBY) { printf(" RUNSTDBY"); }
+        if(OSC32KCTRL->OSC32K.bit.EN1K) { printf(" EN1K"); }
+        if(OSC32KCTRL->OSC32K.bit.EN32K) { printf(" EN32K"); }
+        if(OSC32KCTRL->OSC32K.bit.WRTLOCK) { printf(" WRTLOCK"); }
+        puts("");
+    }
+    if (OSC32KCTRL->OSCULP32K.bit.WRTLOCK) {
+        puts(" OSC32KCTRL->OSCULP32K WRTLOCK");
+    }
+
+    char *rtc_sources[] = { "ULP1K", "ULP32K", "OSC1K", "OSC32K", "XOSC1K", "XOSC32K" };
+    printf(" OSC32KCTRL->RTCCTRL.RTCSEL = %s\n", rtc_sources[OSC32KCTRL->RTCCTRL.bit.RTCSEL]);
+
+    puts("Clock generators:");
+    char *clock_sources[] = {
+        "XOSC", "GCLK_IN", "GCLK_GEN1", "OSCULP32K", "OSC32K",
+        "XOSC32K", "OSC16M", "DFLL48M", "DPLL96M"
+    };
+    for(int i=0; i<9; i++) {
+        if (GCLK->GENCTRL[i].bit.GENEN) {
+            printf(" GCLK->GENCTRL[%02d].SRC = %s", i, clock_sources[GCLK->GENCTRL[i].bit.SRC]);
+            if(GCLK->GENCTRL[i].bit.DIV) {
+                uint16_t div = GCLK->GENCTRL[i].bit.DIV;
+                if(GCLK->GENCTRL[i].bit.DIVSEL) { div = 1 << (div + 1); }
+                printf("/%d", div);
+            }
+            if(GCLK->GENCTRL[i].bit.RUNSTDBY) { printf(" RUNSTDBY"); }
+            if(GCLK->GENCTRL[i].bit.OE) { printf(" OE"); }
+            if(GCLK->GENCTRL[i].bit.OOV) { printf(" OOV"); }
+            if(GCLK->GENCTRL[i].bit.IDC) { printf(" IDC"); }
+            switch(i) {
+                case 0:
+                    printf(" [GCLK_MAIN]");
+                    break;
+                case 1:
+                    printf(" [GCLK_GEN1]");
+                    break;
+                default:
+                    break;
+            }
+            puts("");
+        }
+    }
+    for(int i=0; i<36; i++) {
+        if (GCLK->PCHCTRL[i].bit.CHEN) {
+            printf(" GCLK->PCHCTRL[%02d].SRC = %d", i, GCLK->PCHCTRL[i].bit.GEN);
+            if(GCLK->PCHCTRL[i].bit.WRTLOCK) { printf(" WRTLOCK"); }
+            puts("");
+        }
+    }
+
+    puts("Main clock:");
+    printf(" MCLK->CPUDIV   = GCLK_MAIN/%d\n", MCLK->CPUDIV.reg);
+    printf(" MCLK->AHBMASK  = 0x%08lx\n", MCLK->AHBMASK.reg);
+    printf(" MCLK->APBAMASK = 0x%08lx\n", MCLK->APBAMASK.reg);
+    printf(" MCLK->APBBMASK = 0x%08lx\n", MCLK->APBBMASK.reg);
+    printf(" MCLK->APBCMASK = 0x%08lx\n", MCLK->APBCMASK.reg);
+    printf(" MCLK->APBDMASK = 0x%08lx\n", MCLK->APBDMASK.reg);
+    printf(" MCLK->APBEMASK = 0x%08lx\n", MCLK->APBEMASK.reg);
+
+    puts("Power manager:");
+    printf(" PM->CTRLA.IORET        = %d\n", (PM->CTRLA.bit.IORET) ? 1 : 0);
+    char *sleep_modes[] = { "-", "-", "IDLE", "-", "STANDBY", "BACKUP", "OFF", "-" };
+    printf(" PM->SLEEPCFG.SLEEPMODE = %s\n", sleep_modes[PM->SLEEPCFG.bit.SLEEPMODE]);
+    printf(" PM->PLCFG.PLSEL        = PL%d%s\n", PM->PLCFG.bit.PLSEL, PM->PLCFG.bit.PLDIS ? " PLDIS" : "");
+
+    puts("GPIO:");
+    for (int i=0; i<4; i++) {
+        for (int j=0; j<32; j++) {
+            if (PORT->Group[i].PINCFG[j].reg || (PORT->Group[i].DIR.reg & (1 << j))) {
+                printf(" P%c.%02d:", 'A' + i, j);
+                if (PORT->Group[i].PINCFG[j].bit.PMUXEN) {
+                    printf(" MUX%c", 'A' + (((PORT->Group[i].PMUX[j/2].reg >> 4*(j % 2))) & 0x0f));
+                } else {
+                    if (PORT->Group[i].DIR.reg & (1 << j)){
+                        printf(" OUT value=%d", (PORT->Group[i].OUT.reg & (1 << j)) ? 1 : 0);
+                    }else{
+                        printf(" IN value=%d", (PORT->Group[i].IN.reg & (1 << j)) ? 1 : 0);
+                        if ((PORT->Group[i].PINCFG[j].bit.INEN) == 0) {
+                            printf(" DISABLED ");
+                        }
+                    }
+                    if (PORT->Group[i].PINCFG[j].bit.DRVSTR) {
+                        printf(" DRVSTR ");
+                    }
+                    if (PORT->Group[i].PINCFG[j].bit.PULLEN) {
+                        printf(" PULL%s ", (PORT->Group[i].OUT.reg & (1 << j)) ? "UP" : "DOWN");
+                    }
+                }
+                puts("");
+            }
+        }
+    }
+}
--- main.c
+++ main.c
@@ -41,6 +41,8 @@ static uint16_t emb_address = 1;
 static uint16_t emb_counter = 0;
 static bool emb_sniff = false;
 
+void debug_saml21(void);
+
 int lora_power_cmd(int argc, char **argv)
 {
 
@@ -480,73 +482,7 @@ int debug_cmd(int argc, char **argv)
     (void)argc;
     (void)argv;
 
-    puts("Oscillators:");
-    if (OSCCTRL->XOSCCTRL.reg & 2) { puts(" OSCCTRL->XOSCMCTRL.ENABLE = 1"); }
-    if (OSCCTRL->OSC16MCTRL.reg & 2) {
-        puts(" OSCCTRL->OSC16MCTRL.ENABLE = 1");
-        printf(" OSCCTRL->OSC16MCTRL.FSEL   = %d\n", (OSCCTRL->OSC16MCTRL.reg >> 2) & 0x03);
-    }
-    if (OSCCTRL->DFLLCTRL.reg & 2) { puts(" OSCCTRL->DFLLCTRL.ENABLE = 1"); }
-    if (OSCCTRL->DPLLCTRLA.reg  & 2 ) { puts(" OSCCTRL->DPLLCTRLA.ENABLE = 1"); }
-    if (OSC32KCTRL->XOSC32K.reg & 2) { puts(" OSC32KCTRL->XOSC32K.ENABLE = 1"); }
-    if (OSC32KCTRL->OSC32K.reg & 2) { puts(" OSC32KCTRL->OSC32K.ENABLE  = 1"); }
-    printf(" OSC32KCTRL->RTCCTRL.RTCSEL = %d\n", (uint8_t)(OSC32KCTRL->RTCCTRL.reg  & 0x07));
-
-    puts("Clock generators:");
-    for(int i=0; i<9; i++) {
-        if (GCLK->GENCTRL[i].reg & GCLK_GENCTRL_GENEN) {
-            printf(" GCLK->GENCTRL[%02d].SRC = %d\n", i, (uint8_t)(GCLK->GENCTRL[i].reg & 0x0F));
-        }
-    }
-    for(int i=0; i<35; i++) {
-        if (GCLK->PCHCTRL[i].reg & GCLK_PCHCTRL_CHEN) {
-            printf(" GCLK->PCHCTRL[%02d].SRC = %d\n", i, (uint8_t)(GCLK->PCHCTRL[i].reg & 0x07));
-        }
-    }
-
-    puts("Main clock:");
-    printf(" MCLK->AHBMASK  = 0x%08lx\n", MCLK->AHBMASK.reg);
-    printf(" MCLK->APBAMASK = 0x%08lx\n", MCLK->APBAMASK.reg);
-    printf(" MCLK->APBBMASK = 0x%08lx\n", MCLK->APBBMASK.reg);
-    printf(" MCLK->APBCMASK = 0x%08lx\n", MCLK->APBCMASK.reg);
-    printf(" MCLK->APBDMASK = 0x%08lx\n", MCLK->APBDMASK.reg);
-    printf(" MCLK->APBEMASK = 0x%08lx\n", MCLK->APBEMASK.reg);
-
-
-    puts("Power manager:");
-    printf(" PM->CTRLA.IORET        = %d\n", (PM->CTRLA.reg & 4) ? 1 : 0);
-    printf(" PM->SLEEPCFG.SLEEPMODE = %d\n", PM->SLEEPCFG.reg & 0x07);
-    printf(" PM->PLCFG.PLSEL        = %d\n", PM->PLCFG.reg & 0x03);
-
-    puts("GPIO:");
-    for (int i=0; i<4; i++) {
-        for (int j=0; j<32; j++) {
-            if (PORT->Group[i].PINCFG[j].reg || (PORT->Group[i].DIR.reg & (1 << j))) {
-                printf(" P%c.%02d: ", 'A' + i, j);
-            }
-            if (PORT->Group[i].PINCFG[j].reg & PORT_PINCFG_PMUXEN) {
-                printf("MUX%c", 'A' + (((PORT->Group[i].PMUX[j/2].reg >> 4*(j % 2))) & 0x0f));
-            }else if (PORT->Group[i].DIR.reg & (1 << j)){
-                printf("OUT ");
-                if (PORT->Group[i].PINCFG[j].reg & PORT_PINCFG_DRVSTR) {
-                    printf("DRVSTR ");
-                }
-                printf("value=%d", (PORT->Group[i].OUT.reg & (1 << j)) ? 1 : 0);
-            }else if (PORT->Group[i].PINCFG[j].reg) {
-                printf("IN ");
-                if (PORT->Group[i].PINCFG[j].reg & PORT_PINCFG_PULLEN) {
-                    printf("PULL%s ", (PORT->Group[i].OUT.reg & (1 << j)) ? "UP" : "DOWN");
-                }
-                if ((PORT->Group[i].PINCFG[j].reg & PORT_PINCFG_INEN) == 0) {
-                    printf("DISABLED ");
-                }
-                printf("value=%d", (PORT->Group[i].IN.reg & (1 << j)) ? 1 : 0);
-            }
-            if (PORT->Group[i].PINCFG[j].reg || (PORT->Group[i].DIR.reg & (1 << j))) {
-                printf("\n");
-            }
-        }
-    }
+    debug_saml21();
     return 0;
 }
 #endif
